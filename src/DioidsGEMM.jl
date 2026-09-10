497e285c7118eb419e08e8564e7a90a2e83b8c46
Sole <30993566+sonosole@users.noreply.github.com>
2026-09-10 22:49:01 +0800

    feat: show for array in Dioid

--- src/DioidsGEMM.jl
+++ src/DioidsGEMM.jl
@@ -3,6 +3,12 @@ module DioidsGEMM
 using Dioids
 using LoopVectorization
 
+
+function Base.show(io::IO, x::Dioid{⨁, ⨀, A}) where {⨁, ⨀, A <: AbstractArray}
+    print(io, "Dioid{$⨁,$⨀} contains ")
+    display(x.data)
+end
+
 include("0-matmat.jl")
 include("1-matvec.jl")
 include("2-vecmat.jl")
